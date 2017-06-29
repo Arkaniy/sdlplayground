7f301264313c192b33cce9e7c3c934e8dfd24c9b
Arkaniy <lexaalek@mail.ru>
2017-06-30 00:01:25 +0300

    add scene

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "gfxengine/resourcepreloadholder.h"
 
 #include "core/container.h"
+#include "core/scene.h"
 
 #include "core/components/sprite.h"
 #include "core/components/transform.h"
@@ -15,20 +16,32 @@ int main() {
 
     ResourcePreloadHolder resourcePreloadHolder;
     resourcePreloadHolder.addImage("img.jpg", "bg");
+    resourcePreloadHolder.addImage("rb.png", "bg1");
+
+    Scene scene;
 
     Container container;
 
     Sprite sprite(container, "bg");
-    Transform transform(container, 0, 0, 720, 480);
+    Transform transform(container, 0, 0, 480, 320);
+
+    Container container1;
+
+    Sprite sprite1(container1, "bg1");
+    Transform transform1(container1, 100, 100, 480, 320);
+
+    container.addComponent(std::make_shared<Sprite>(sprite));
+    container.addComponent(std::make_shared<Transform>(transform));
+
+    container1.addComponent(std::make_shared<Sprite>(sprite1));
+    container1.addComponent(std::make_shared<Transform>(transform1));
 
     gfxEngine.loadResources(resourcePreloadHolder);
 
-    gfxEngine.startFrame();
-    gfxEngine.drawSprite(&sprite, &transform);
-    gfxEngine.endFrame();
+    scene.registerEntity(std::make_shared<Container>(container));
+    scene.registerEntity(std::make_shared<Container>(container1));
 
-
-    std::cin.get();
+    scene.run();
 
     gfxEngine.free();
 }
